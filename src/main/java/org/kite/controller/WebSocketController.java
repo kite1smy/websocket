3728edd9ebf361508c02b35f1a37e518fd5cdc93
shaomingyang <shaomingyang@phoenixtv.com>
2020-01-13 19:04:11 +0800

    rxjava test

--- src/main/java/org/kite/controller/WebSocketController.java
+++ src/main/java/org/kite/controller/WebSocketController.java
@@ -1,5 +1,7 @@
 package org.kite.controller;
 
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.BehaviorSubject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,7 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -24,56 +27,95 @@ public class WebSocketController {
     /**
      * 用来记录当前连接数的变量
      */
-    private static AtomicInteger onlineCount = new AtomicInteger();
+    private static OnlineCount onlineCount = new OnlineCount();
 
     /**
      * 保存 组id->组成员 的映射关系
      */
     private static ConcurrentHashMap<String, List<Session>> groupMemberInfoMap = new ConcurrentHashMap<>();
 
-    @OnOpen
-    public void onOpen(Session session, @PathParam("id") String id, @PathParam("name") String name) throws Exception {
-        List<Session> sessionList = groupMemberInfoMap.computeIfAbsent(id, k -> new ArrayList<>());
-        sessionList.add(session);
 
-        log.info("Connection connected");
-        log.info("sid: {}, sessionList size: {}", id, sessionList.size());
-
-        onlineCount.incrementAndGet();
-    }
-
-    @OnClose
-    public void onClose(Session session, @PathParam("id") String id) {
-
+    private void broadcast(String id, String msg) {
         List<Session> sessionList = groupMemberInfoMap.get(id);
-        if (sessionList == null) {
-            log.warn("没有参数");
-            return;
-        }
-        sessionList.remove(session);
-        log.info("Connection closed");
-        log.info("sid: {}, sessionList size: {}", id, sessionList.size());
-
-        onlineCount.decrementAndGet();
-    }
-
-    @OnMessage
-    public void onMessage(String message, Session session, @PathParam("id") String id, @PathParam("name") String name) throws IOException {
-        List<Session> sessionList = groupMemberInfoMap.get(id);
-        // 先一个群组内的成员发送消息
-        sessionList.forEach(item -> {
+        sessionList.forEach(session -> {
             try {
-                String text = name + ": " + message;
-                item.getBasicRemote().sendText(text);
+                Optional.of(session)
+                        .map(Session::getBasicRemote)
+                        .get()
+                        .sendText(msg);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         });
     }
 
+    @OnOpen
+    public void onOpen(Session session, @PathParam("id") String id, @PathParam("name") String name) throws Exception {
+        List<Session> sessionList = groupMemberInfoMap.computeIfAbsent(id, k -> new ArrayList<>());
+        sessionList.add(session);
+
+        log.info("Connection connected  sid: {}, sessionList size: {}", id, sessionList.size());
+
+        onlineCount.incrementAndGet();
+        onlineCount.subscribe(session);
+    }
+
+    @OnClose
+    public void onClose(Session session, @PathParam("id") String id) {
+
+        List<Session> sessionList = groupMemberInfoMap.get(id);
+
+        log.info("Connection closed sid: {}, sessionList size: {}", id, sessionList.size());
+
+        sessionList.remove(session);
+        onlineCount.decrementAndGet();
+
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session, @PathParam("id") String id, @PathParam("name") String name) throws IOException {
+        List<Session> sessionList = groupMemberInfoMap.get(id);
+        broadcast(id, name + " : " + message);
+    }
+
     @OnError
     public void onError(Session session, Throwable error) {
         log.error("Error while websocket. ", error);
+    }
+
+    private static class OnlineCount {
+        private static AtomicInteger onlineCount = new AtomicInteger();
+
+        private BehaviorSubject<Integer> subject = BehaviorSubject.create();
+
+        void subscribe(Session session) {
+
+            Disposable dispose;
+            dispose = subject.subscribe(integer -> {
+                if (session.isOpen()) {
+                    Optional.of(session)
+                            .map(Session::getBasicRemote)
+                            .get()
+                            .sendText("在线人数: " + integer);
+                } else {
+//                    dispose.dispose();
+                }
+            });
+
+
+        }
+
+        void incrementAndGet() {
+            subject.onNext(
+                    onlineCount.incrementAndGet()
+            );
+        }
+
+        void decrementAndGet() {
+            subject.onNext(
+                    onlineCount.decrementAndGet()
+            );
+        }
     }
 
 }
--- src/main/java/org/kite/websocket/MsgHandler.java
+++ src/main/java/org/kite/websocket/MsgHandler.java
@@ -1,9 +1,15 @@
 package org.kite.websocket;
 
+import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 
 public class MsgHandler extends SimpleChannelInboundHandler<Object> {
@@ -18,7 +24,7 @@ public class MsgHandler extends SimpleChannelInboundHandler<Object> {
         System.out.println("收到客户端消息, msg = " + msg);
 
         // WebSocket 二进制消息会通过 HttpServerCodec 解码成 BinaryWebSocketFrame 类对象
-        TextWebSocketFrame frame = (TextWebSocketFrame)msg;
+        TextWebSocketFrame frame = (TextWebSocketFrame) msg;
         ByteBuf byteBuf = frame.content();
 
         // 拿到真实的字节数组并打印
@@ -36,5 +42,56 @@ public class MsgHandler extends SimpleChannelInboundHandler<Object> {
         String myStr = new String(byteArray);
 
         System.out.println(myStr);
+    }
+
+    /**
+     * 服务端
+     */
+    public static class SocketServer {
+
+        public static void main(String[] args) {
+            EventLoopGroup bossGroup = new NioEventLoopGroup();
+            EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+            ServerBootstrap b = new ServerBootstrap();
+
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);  // 服务器信道的处理方式
+
+            b.childHandler(new ChannelInitializer<SocketChannel>() { // 客户端信道的处理器方式
+                @Override
+                protected void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(
+                            new HttpServerCodec(), // Http 服务器编解码器
+                            new HttpObjectAggregator(65535), // 内容长度限制
+                            new WebSocketServerProtocolHandler("/"), // WebSocket 协议处理器, 在这里处理握手、ping、pong 等消息
+                            new MsgHandler() // 自定义的消息处理器
+
+
+                    );
+                }
+
+            });
+
+
+            try {
+                // 绑定 12345 端口,
+                ChannelFuture f = b.bind(12345).sync();
+
+                if (f.isSuccess()) {
+                    System.out.println("服务器启动成功");
+                }
+
+                // 等待服务器信道关闭,
+                // 也就是不要退出应用程序,
+                // 让应用程序可以一直提供服务
+                f.channel().closeFuture().sync();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                bossGroup.shutdownGracefully();
+                workerGroup.shutdownGracefully();
+            }
+        }
     }
 }
